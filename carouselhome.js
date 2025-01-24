f677b7bc3a1244eecebb8e051af9b9a7a73fb95c
LLetzel <lucasdiasletzel@gmail.com>
2025-01-24 10:44:33 -0300

    24/01

--- carouselhome.js
+++ carouselhome.js
@@ -1,26 +1,31 @@
-
-const carrossel = document.querySelector('.carrossel');
-const carrosselItens = document.querySelectorAll('.carrossel-item');
-const carrosselPrev = document.querySelector('.carrossel-prev');
-const carrosselNext = document.querySelector('.carrossel-next');
-let indiceAtual = 0;
-
-carrosselItens[indiceAtual].classList.add('ativo');
-
-carrosselPrev.addEventListener('click', () => {
-  indiceAtual--;
-  if (indiceAtual < 0) {
-    indiceAtual = carrosselItens.length - 1;
-  }
-  carrosselItens.forEach((item) => item.classList.remove('ativo'));
-  carrosselItens[indiceAtual].classList.add('ativo');
-});
-
-carrosselNext.addEventListener('click', () => {
-  indiceAtual++;
-  if (indiceAtual >= carrosselItens.length) {
-    indiceAtual = 0;
-  }
-  carrosselItens.forEach((item) => item.classList.remove('ativo'));
-  carrosselItens[indiceAtual].classList.add('ativo');
+document.addEventListener("DOMContentLoaded", function() {
+    const swiper = new Swiper(".mySwiper", {
+        effect: "fade", // fade effect between slides
+        loop: true, // infinite loop
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        speed: 800, // transition speed
+        grabCursor: true,
+        fadeEffect: {
+            crossFade: true
+        },
+        keyboard: {
+            enabled: true,
+        },
+        on: {
+            init: function() {
+                document.querySelector('.swiper').style.opacity = 1;
+            }
+        }
+    });
 });
